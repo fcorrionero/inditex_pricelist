0ecec02f91ea3042a0b02b04667e9838b1c9da05
fcorrionero <fcorrionero@gmail.com>
2022-07-06 14:17:19 +0200

    h2 fixtures

--- src/main/java/com/fcorrionero/inditex/infrastructure/persistence/repository/H2PriceRepository.java
+++ src/main/java/com/fcorrionero/inditex/infrastructure/persistence/repository/H2PriceRepository.java
@@ -5,8 +5,11 @@ import com.fcorrionero.inditex.domain.repository.PriceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class H2PriceRepository implements PriceRepository {
@@ -15,9 +18,10 @@ public class H2PriceRepository implements PriceRepository {
     private JdbcTemplate jdbcTemplate;
 
     @Override
-    public List<ProductPrice> findPricesByApplicationDateProductIdAndBrandId(Date appDate, int productId, int brandId) {
-        jdbcTemplate.query("Select * From Product_Price", new ProductPriceMapper());
+    public List<ProductPrice> findPricesByApplicationDateProductIdAndBrandId(Date appDate, int productId, int brandId)
+    {
+        return jdbcTemplate.query("Select * From Product_Price", new ProductPriceMapper());
 
-        return null;
+//        return List.of();
     }
 }
--- src/main/java/com/fcorrionero/inditex/infrastructure/persistence/repository/ProductPriceMapper.java
+++ src/main/java/com/fcorrionero/inditex/infrastructure/persistence/repository/ProductPriceMapper.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.UUID;
 
 
@@ -12,6 +13,32 @@ public class ProductPriceMapper implements RowMapper<ProductPrice> {
 
     @Override
     public ProductPrice mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return null;
+        try{
+//            String fieldID = rs.getString("ID");
+//            UUID id = UUID.fromString(fieldID);
+            UUID id = UUID.randomUUID();
+            int brandId = rs.getInt("BRAND_ID");
+            int priceList = rs.getInt("PRICE_LIST");
+            int productId = rs.getInt("PRODUCT_ID");
+            int priority = rs.getInt("PRIORITY");
+            float price = rs.getFloat("PRICE");
+            String currency = rs.getString("CURRENCY");
+            Date startDate = rs.getDate("START_DATE");
+            Date endDate = rs.getDate("END_DATE");
+            return new ProductPrice(
+                    id,
+                    brandId,
+                    startDate,
+                    endDate,
+                    priceList,
+                    productId,
+                    priority,
+                    price,
+                    currency
+            );
+        }catch (RuntimeException e){
+            return null; //TODO Add proper error handling
+        }
+
     }
 }
